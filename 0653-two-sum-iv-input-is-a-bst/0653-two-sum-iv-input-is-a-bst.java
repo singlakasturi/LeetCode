e9022281346c545d54a1d6209d950aff58edddeb
Kasturi Singla <158548625+singlakasturi@users.noreply.github.com>
2026-07-03 11:11:38 +0530

    Time: 1 ms (100%), Space: 47.4 MB (21.68%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -11,24 +11,42 @@
  *         this.left = left;
  *         this.right = right;
  *     }
- 
  * }
  */
 class Solution {
 
-    HashSet<Integer> set = new HashSet<>();
+    public int size(TreeNode root) {
+        if(root == null)
+            return 0;
 
+        return 1 + size(root.left) + size(root.right);
+    }
 
+    public void fillArr(int[] arr, TreeNode node, int[] ind) {
+        if(node == null)
+            return ;
+
+        fillArr(arr, node.left, ind);
+        arr[ind[0]++] = node.val;
+        fillArr(arr, node.right, ind);
+    }
 
     public boolean findTarget(TreeNode root, int k) {
-        if(root == null)
-            return false;
-        
-        if(set.contains(k - root.val))
-            return true;
+        int[] arr = new int[size(root)];
+        fillArr(arr, root, new int[]{0});
 
-        set.add(root.val);
-        
-        return findTarget(root.left, k) || findTarget(root.right, k);
+        int n = arr.length;
+        int l = 0, r = n-1;
+
+        while(l < r) {
+            if(arr[l] + arr[r] == k)
+                return true;
+            else if(arr[l] + arr[r] < k)
+                l++;
+            else
+                r--;
+        }
+
+        return false;
     }
 }
