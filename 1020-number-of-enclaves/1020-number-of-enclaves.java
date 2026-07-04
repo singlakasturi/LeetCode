8060ab95731d617052922de8370e7c132c399010
Kasturi Singla <158548625+singlakasturi@users.noreply.github.com>
2026-07-04 19:50:01 +0530

    Time: 9 ms (51.92%), Space: 60.8 MB (43.15%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -2,25 +2,14 @@ class Solution {
 
     int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-    public void bfs(int[][] grid, int i, int j, int n, int m, boolean[][] vis) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{i, j});
+    public void dfs(int[][] grid, int i, int j, int n, int m, boolean[][] vis) {
+        if(i < 0 || i >= n || j < 0 || j >= m || vis[i][j] || grid[i][j] != 1)
+            return ;
+
         vis[i][j] = true;
 
-        while(!q.isEmpty()) {
-            int[] node = q.poll();
-
-            for(int k=0;k<4;k++) {
-                int newR = node[0] + dir[k][0];
-                int newC = node[1] + dir[k][1];
-
-                if(newR < 0 || newR >= n || newC < 0 || newC >= m || vis[newR][newC] || grid[newR][newC] != 1)
-                    continue;
-
-                q.offer(new int[]{newR, newC});
-                vis[newR][newC] = true;
-            }
-        }
+        for(int k=0;k<4;k++)
+            dfs(grid, i+dir[k][0], j+dir[k][1], n, m, vis);
     }
 
     public int numEnclaves(int[][] grid) {
@@ -31,16 +20,16 @@ class Solution {
 
         for(int i=0;i<n;i++) {
             if(grid[i][0] == 1 && !vis[i][0])
-                bfs(grid, i, 0, n, m, vis);
+                dfs(grid, i, 0, n, m, vis);
             if(grid[i][m-1] == 1 && !vis[i][m-1])
-                bfs(grid, i, m-1, n, m, vis);
+                dfs(grid, i, m-1, n, m, vis);
         }
 
         for(int j=0;j<m;j++) {
             if(grid[0][j] == 1 && !vis[0][j])
-                bfs(grid, 0, j, n, m, vis);
+                dfs(grid, 0, j, n, m, vis);
             if(grid[n-1][j] == 1 && !vis[n-1][j])
-                bfs(grid, n-1, j, n, m, vis);
+                dfs(grid, n-1, j, n, m, vis);
         }
 
         int ans = 0;
