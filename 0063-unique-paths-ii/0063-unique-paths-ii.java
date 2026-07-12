800be4de7123823f410bbc4a763383564c4b71dd
Kasturi Singla <158548625+singlakasturi@users.noreply.github.com>
2026-07-12 12:31:26 +0530

    Time: 0 ms (100%), Space: 43.7 MB (27.07%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,6 +1,6 @@
 class Solution {
 
-    int[][] dp;
+    int[] dp;
 
     // public int solve(int i, int j, int[][] o, int n, int  m) {
     //     if(i >= n || j >= m || o[i][j] == 1)
@@ -25,29 +25,22 @@ class Solution {
         if(o[0][0] == 1 || o[n-1][m-1] == 1)
             return 0;
 
-        dp = new int[n][m];
+        dp = new int[m];
         
-        for(int i=0;i<n;i++)
-            if(o[i][0] == 0)
-                dp[i][0] = 1;
-            else
-                break;
-        for(int j=0;j<m;j++)
-            if(o[0][j] == 0)
-                dp[0][j] = 1;
-            else
-                break;
+        dp[0] = 1;
 
-        for(int i=1;i<n;i++) {
+        for(int i=0;i<n;i++) {
+            if(o[i][0] == 1)
+                dp[0] = 0;
             for(int j=1;j<m;j++) {
                 if(o[i][j] == 1)
-                    continue;
-
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                    dp[j] = 0;
+                else
+                    dp[j] = dp[j] + dp[j-1];
             }
         }
 
-        return dp[n-1][m-1];
+        return dp[m-1];
 
     }
 }
