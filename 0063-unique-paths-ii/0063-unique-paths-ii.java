f203b49178c687457285fff94a8886d6dcfdd352
Kasturi Singla <158548625+singlakasturi@users.noreply.github.com>
2026-07-12 12:25:19 +0530

    Time: 0 ms (100%), Space: 43.5 MB (64.27%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,30 +2,52 @@ class Solution {
 
     int[][] dp;
 
-    public int solve(int i, int j, int[][] o, int n, int  m) {
-        if(i >= n || j >= m || o[i][j] == 1)
-            return 0;
+    // public int solve(int i, int j, int[][] o, int n, int  m) {
+    //     if(i >= n || j >= m || o[i][j] == 1)
+    //         return 0;
 
-        if(i == n-1 && j == m-1)
-            return 1;
+    //     if(i == n-1 && j == m-1)
+    //         return 1;
 
-        if(dp[i][j] != -1)
-            return dp[i][j];
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
 
-        int right = solve(i+1, j, o, n, m);
-        int down = solve(i, j+1, o, n, m);
+    //     int right = solve(i+1, j, o, n, m);
+    //     int down = solve(i, j+1, o, n, m);
 
-        return dp[i][j] = right + down;
-    }
+    //     return dp[i][j] = right + down;
+    // }
 
     public int uniquePathsWithObstacles(int[][] o) {
         int n = o.length;
         int m = o[0].length;
 
-        dp = new int[n][m];
-        for(int[] i : dp)
-            Arrays.fill(i, -1);
+        if(o[0][0] == 1 || o[n-1][m-1] == 1)
+            return 0;
 
-        return solve(0, 0, o, n, m);
+        dp = new int[n][m];
+        
+        for(int i=0;i<n;i++)
+            if(o[i][0] == 0)
+                dp[i][0] = 1;
+            else
+                break;
+        for(int j=0;j<m;j++)
+            if(o[0][j] == 0)
+                dp[0][j] = 1;
+            else
+                break;
+
+        for(int i=1;i<n;i++) {
+            for(int j=1;j<m;j++) {
+                if(o[i][j] == 1)
+                    continue;
+
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        return dp[n-1][m-1];
+
     }
 }
