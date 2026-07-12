cb72ef5e2438c81dda7372f8054ef8584957c3af
Kasturi Singla <158548625+singlakasturi@users.noreply.github.com>
2026-07-12 13:04:12 +0530

    Time: 3 ms (73.85%), Space: 50.4 MB (21.22%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,6 +1,6 @@
 class Solution {
 
-    int[][] dp;
+    int[] dp;
 
     // public int solve(int i, int j, int[][] grid, int n, int m) {
     //     if(i >= n || j >= m)
@@ -26,21 +26,21 @@ class Solution {
         int n = grid.length;
         int m = grid[0].length;
 
-        dp = new int[n][m];
-        dp[0][0] = grid[0][0];
+        dp = new int[m];
+        dp[0] = grid[0][0];
 
-        for(int i=1;i<n;i++)
-            dp[i][0] = grid[i][0] + dp[i-1][0];
+
         for(int j=1;j<m;j++)
-            dp[0][j] = grid[0][j] + dp[0][j-1];
+            dp[j] = grid[0][j] + dp[j-1];
 
         for(int i=1;i<n;i++) {
+            dp[0] = grid[i][0] + dp[0];
             for(int j=1;j<m;j++) {
-                int ans = Math.min(dp[i-1][j], dp[i][j-1]);
-                dp[i][j] = grid[i][j] + ans;
+                int ans = Math.min(dp[j], dp[j-1]);
+                dp[j] = grid[i][j] + ans;
             }
         }
 
-        return dp[n-1][m-1];
+        return dp[m-1];
     }
 }
