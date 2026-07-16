aeafe3d3cca8ba6a90cbf9f25e17da7f259670bb
Kasturi Singla <158548625+singlakasturi@users.noreply.github.com>
2026-07-16 16:53:38 +0530

    Time: 5 ms (68.69%), Space: 47.2 MB (34.75%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,40 +1,58 @@
 class Solution {
 
-    Integer[][] dp;
+    int[][] dp;
 
-    public int solve(int i, int j, String a, String b, int n, int m) {
-        if(j == m)
-            return n - i;
-        if(i == n)
-            return m - j;
+    // public int solve(int i, int j, String a, String b, int n, int m) {
+    //     if(j == m)
+    //         return n - i;
+    //     if(i == n)
+    //         return m - j;
 
-        if(dp[i][j] != null)
-            return dp[i][j];
+    //     if(dp[i][j] != null)
+    //         return dp[i][j];
 
-        int ans = Integer.MAX_VALUE;
+    //     int ans = int.MAX_VALUE;
 
-        if(a.charAt(i) == b.charAt(j))
-            ans = Math.min(ans, solve(i+1, j+1, a, b, n, m));
-        else {
-            ans = Math.min(
-                ans, 
-                1 + Math.min(
-                    Math.min(
-                        solve(i, j+1, a, b, n, m), 
-                        solve(i+1, j, a, b, n, m))
-                , solve(i+1, j+1, a, b, n, m))
-            );
-        }
+    //     if(a.charAt(i) == b.charAt(j))
+    //         ans = Math.min(ans, solve(i+1, j+1, a, b, n, m));
+    //     else {
+    //         ans = Math.min(
+    //             ans, 
+    //             1 + Math.min(
+    //                 Math.min(
+    //                     solve(i, j+1, a, b, n, m), 
+    //                     solve(i+1, j, a, b, n, m))
+    //             , solve(i+1, j+1, a, b, n, m))
+    //         );
+    //     }
 
-        return dp[i][j] = ans;
-    }
+    //     return dp[i][j] = ans;
+    // }
 
     public int minDistance(String a, String b) {
         int n = a.length();
         int m = b.length();
 
-        dp = new Integer[n][m];
+        dp = new int[n + 1][m + 1];
 
-        return solve(0, 0, a, b, n, m);
+        for(int i=0;i<=m;i++)
+            dp[0][i] = i;
+        for(int i=0;i<=n;i++)
+            dp[i][0] = i;
+
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                int ans = Integer.MAX_VALUE;
+                if(a.charAt(i-1) == b.charAt(j-1))
+                    ans = Math.min(ans, dp[i-1][j-1]);
+                else {
+                    ans = 1 + Math.min(ans, Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1])));
+                }
+
+                dp[i][j] = ans;
+            }
+        }
+
+        return dp[n][m];
     }
 }
