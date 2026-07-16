384172e3e4f9e1a1eb80734bc0af6e1b00e44370
Kasturi Singla <158548625+singlakasturi@users.noreply.github.com>
2026-07-16 16:37:54 +0530

    Time: 20 ms (55.79%), Space: 54.3 MB (53.4%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,30 +1,42 @@
 class Solution {
 
-    Integer[][] dp;
+    int[][] dp;
 
-    public int solve(int i, int j, String s, String t, int n, int m){ 
-        if(j == m)
-            return 1;
-        if(i >= n)
-            return 0;
+    // public int solve(int i, int j, String s, String t, int n, int m){ 
+    //     if(j == m)
+    //         return 1;
+    //     if(i >= n)
+    //         return 0;
 
-        if(dp[i][j] != null)
-            return dp[i][j];
+    //     if(dp[i][j] != null)
+    //         return dp[i][j];
 
-        int take = s.charAt(i) == t.charAt(j) ? solve(i+1, j+1, s, t, n, m) : 0;
-        int notTake = solve(i+1, j, s, t, n, m);
+    //     int take = s.charAt(i) == t.charAt(j) ? solve(i+1, j+1, s, t, n, m) : 0;
+    //     int notTake = solve(i+1, j, s, t, n, m);
 
-        long ans = take + notTake;
+    //     long ans = take + notTake;
 
-        return dp[i][j] = (int) ans;
-    }
+    //     return dp[i][j] = (int) ans;
+    // }
 
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
 
-        dp = new Integer[n][m];
+        dp = new int[n+1][m+1];
 
-        return solve(0, 0, s, t, n, m);
+        for(int i=0;i<=n;i++)
+            dp[i][0] = 1;
+
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                int take = s.charAt(i-1) == t.charAt(j-1) ? dp[i-1][j-1] : 0;
+                int notTake = dp[i-1][j];
+
+                dp[i][j] = take + notTake;
+            }
+        }
+
+        return dp[n][m];
     }
 }
