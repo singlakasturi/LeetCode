e5c2d9ef4205044e145566a2a216060d47886ac8
Kasturi Singla <158548625+singlakasturi@users.noreply.github.com>
2026-07-21 12:31:31 +0530

    Time: 1 ms (70.36%), Space: 43.7 MB (32.25%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -2,28 +2,18 @@ class Solution {
 
     int[][] dp;
 
-    public int solve(int i, int[] prices, int canBuy, int n) {
-        if(i >= n)
-            return 0;
-
-        if(dp[i][canBuy] != -1)
-            return dp[i][canBuy];
-
-        if(canBuy == 1)
-            dp[i][canBuy] = Math.max(solve(i+1, prices, canBuy, n), solve(i+1, prices, 0, n) - prices[i]);
-        else
-            dp[i][canBuy] = Math.max(solve(i+1, prices, canBuy, n), solve(i+2, prices, 1, n) + prices[i]);
-
-        return dp[i][canBuy]; 
-    }
-
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        dp = new int[n][2];
+        dp = new int[n + 2][2];
 
-        for(int[] i : dp)
-            Arrays.fill(i, -1);
+        dp[0][1] = 0;
+        dp[1][1] = -prices[0];
 
-        return solve(0, prices, 1, n);
+        for(int i=2;i<=n+1;i++) {
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-2][0] - prices[i-2]);
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i-2]);
+        }
+
+        return dp[n+1][0];
     }
 }
