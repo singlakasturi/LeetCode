6bcf02709c4ca111a76ff9c2d0291aff3089eecf
Kasturi Singla <158548625+singlakasturi@users.noreply.github.com>
2026-09-19 15:39:35 +0530

    Time: 2 ms (45.56%), Space: 94.8 MB (15.67%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,16 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
+        int profit = 0;
         int price = prices[0];
-        int ans = 0;
 
         for(int i=1;i<n;i++) {
-            int profit = prices[i] - price;
-            ans = Math.max(ans, profit);
+            if(prices[i] - price >= 0) {
+                profit = Math.max(profit, prices[i] - price);
+            }
 
             price = Math.min(price, prices[i]);
         }
 
-        return ans;
+        return profit;
     }
 }
